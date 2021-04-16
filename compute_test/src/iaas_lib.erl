1f168b09f27f4c342da101b74db27e5b6841d3a5
joq62 <joakim.leche@gmail.com>
2021-04-16 23:01:38 +0200

    iaas ud

--- compute_test/src/iaas_lib.erl
+++ compute_test/src/iaas_lib.erl
@@ -37,8 +37,28 @@ get_catalog()->
 %% Description:
 %% Returns: non
 %% --------------------------------------------------------------------
-% Catalog [{boardNode,[slaveNodes],ErlCmd}
+% Catalog [{boardNode,[{slaveNodes,ErlCmd}]}
 start_stop_nodes()->
     {ok,Catalog}=file:consult(?Catalog),
+    RunningBoardNodes=[{BoardNode,SlaveList}||{BoardNode,SlaveList}<-Catalog,
+				  pong==net_adm:ping(BoardNode)],
+    MissingBoardNodes=[BoardNode||{BoardNode,_}<-Catalog,
+				  false==lists:member(BoardNode,RunningBoardNodes)],
     AllRunningNodes=nodes(),
+    {StartResult,StopResult}=slaves_to_start_stop([RunningBoardNodes,AllRunningNodes,[],[]),
+    
+    ok.
+
+
+slaves_to_start_stop([{RunningBoardNode,Slaves}|T],AllRunningNodes,AccBoards,AccSlaves)->
+    StartSlaves=[{RunningBoardNode,string:tokens(atom_to_list(SlaveNode),"@"),ERlCmd}||{SlaveNode,ERlCmd}<-Slaves,
+						     nodes pang==net_adm:ping(SlaveNode)],
+
+    StopSlaves=[SlaveNode||Node<-AllRunningNodes,
+			   false==lists:keymember(Node,1,Slaves)],
+    StartSlavesResult=[rpc:call(RunningBoardNode,slave,start,[Host,SlaveNodeId,ErlCmd])||
+			  {RunningBoardNode,[SlaveNodeId,Host],ERlCmd}<-StartSlaves],
+
+    rpc
+    
     
